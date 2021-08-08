3f8321c02deb5241d6bbf6da87dc44e7afd13180
Robin Joseph <48469274+rj-robinjoseph@users.noreply.github.com>
2021-08-08 17:09:10 +0530

    Login & Register bug fixed.

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -5,8 +5,7 @@ import { useContext } from "react";
 import { logout } from "../../context/authContext/AuthActions";
 import { AuthContext } from "../../context/authContext/AuthContext";
 import { useHistory } from "react-router-dom";
-import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
 
 export default function Topbar() {
   const {dispatch} = useContext(AuthContext);
@@ -35,13 +34,7 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
-          <span className="logout" onClick={logoutHandler}>Logout</span>
-          <img
-            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="topAvatar"
-          />
+          <img onClick={logoutHandler} src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
