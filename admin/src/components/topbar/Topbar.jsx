5516b5f987d2e10c62da833866366d418c01c1dd
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-08-08 16:22:15 +0500

    logout and register page bug fix

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import React from "react"
+import "./topbar.css"
+import { NotificationsNone, Language, Settings } from "@material-ui/icons"
 
 export default function Topbar() {
   return (
@@ -21,9 +21,13 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>
-  );
+  )
 }
